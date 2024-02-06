63a549afe1e3456495aae77e0dc2802799e1e907
Dasha Demidova <striki06@mail.ru>
2024-02-06 22:13:45 +0300

    update

--- C.py
+++ C.py
@@ -4,21 +4,10 @@ lines = stdin.readlines()
 
 
 def comments_clean(data):
-    is_comments = False
-    clean_text = []
-    for line in data:
-        new_line = ''
-        for letter in line:
-            if letter == '#':  # коммент
-                is_comments = True
-            elif not is_comments or letter == '\n':
-                new_line += letter
-            if letter == '\n':
-                is_comments = False
-        new_line = (''.join(new_line)).rstrip()
-        if new_line:
-            clean_text.append(new_line)
-    print('\n'.join(clean_text).lstrip('\n'))
+    for idx, line in enumerate(data):
+        if '#' in line:
+            data[idx] = line[:line.index('#')].rstrip()
+    print('\n'.join(data).lstrip('\n'))
 
 
 comments_clean(lines)  # commments
